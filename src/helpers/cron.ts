5ec8092700107758125206ef4e58bb092f1bd8ca
Dante Ramírez <danter.ram@gmail.com>
2022-10-08 12:21:27 -0500

    Change node-fetch to axios

--- src/helpers/cron.ts
+++ src/helpers/cron.ts
@@ -1,19 +1,28 @@
-import logger from './logger';
+import axios from 'axios';
 import config from '../../configuration';
+import logger from './logger';
 
 const cron = require('node-cron');
-const fetch = require('node-fetch');
 
 const { apiBaseUrl } = config;
 
 class Cron {
   static async enablePoints() {
-    fetch(`${apiBaseUrl}/api/v1/public/wallets/enable-points`, {
-      method: 'PUT'
-    })
-      .then((Response: { json: () => any; }) => Response.json())
-      .then((data: any) => logger.log(data))
-      .catch((error: any) => logger.error('Error:', error));
+    let res: any;
+
+    try {
+      res = await axios.put(`${apiBaseUrl}/api/v1/public/wallets/enable-points`);
+    } catch (error: any) {
+      if (error.response) {
+        logger.error('ErrorResponse:', { status: error.response.status, statusText: error.response.statusText, code: error.response.data.code });
+      } else if (error.request) {
+        logger.error('ErrorRequest:', error.request);
+      }
+
+      return logger.error('ErrorMessage:', error.message);
+    }
+
+    return logger.log({ status: res.status, statusText: res.statusText, data: res.data });
   }
 }
 
